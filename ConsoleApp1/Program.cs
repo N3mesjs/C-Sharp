0ac0a30cb1dcae4b192fc0b91ec1b933ab41d17c
N3mesjs <84539974+N3mesjs@users.noreply.github.com>
2024-11-22 16:03:31 +0100

    initial commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,19 @@ namespace ConsoleApp1
   {
     static void Main(string[] args)
     {
-      Console.Beep();
+      Random random = new Random();
+      const String name = "Bro";
+      Console.WriteLine("CHCIO\nawda\t"+name); //escape sequense: \(lettera in base alla funzione che serve)
+
+      double a = 3.14;
+      int b = Convert.ToInt32(a);
+      Console.WriteLine(b);
+
+      double num = Math.Pow(3, 2);
+      Console.WriteLine(num);
+      //Console.ReadKey(); //prendo in input una lettera
+
+      Console.WriteLine(random.Next());
     }
   }
 }
--- ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c2c240529341a1c0ef734b7f76b4336866b0c76")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
